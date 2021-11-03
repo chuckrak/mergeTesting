5aee9583351b24902309aa17134ac5c3b6dbe494
chuckrak <chuckrak1@gmail.com>
2021-11-03 13:30:28 -0700

    Hello

---
+++ helloWorld.py
@@ -0,0 +1,9 @@
+class HelloWorld:
+    def __init__(self, message):
+        self.message = message
+    def printMessage(self):
+        print(self.message)
+
+world = HelloWorld("hi there!")
+world.printMessage()
+        
